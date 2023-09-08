970fd8979a0de7a57e35379a89459f61897431da
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-08 15:38:53 +0300

    superb-kitchen

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,21 +1,16 @@
-import React from 'react'
+import React from "react";
 import { AiOutlineEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
-
-function Login(){
-
-    return(
-        <>
-
-<section className="register">
+function Login() {
+  return (
+    <>
+      <section className="register">
         <div className="regsiter-container">
           <p className="home-link title">Superb Kitchen</p>
           <p className="register-text">Login</p>
 
           <form className="form">
-           
-
             <div className="form-container">
               <div className="name-label">
                 <label>Email</label>
@@ -24,8 +19,6 @@ function Login(){
                 <input type="email" placeholder="enter your Email" />
               </div>
             </div>
-
-           
 
             <div className="form-container">
               <div className="name-label">
@@ -37,20 +30,19 @@ function Login(){
               </div>
             </div>
 
-            <button type="submit" className="submit-btn">
-              Let's Go
-            </button>
+            <Link to="/">
+              <button type="submit" className="submit-btn">
+                Let's Go
+              </button>
+            </Link>
             <Link to="/login" className="login-p">
               <p>Not yet Registered? Register</p>
             </Link>
           </form>
         </div>
       </section>
-
-
-
-        </>
-    )
+    </>
+  );
 }
 
-export default Login
+export default Login;
