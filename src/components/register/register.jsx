5f88f6489634e2b009bd40fdb029948ffaaf4433
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-08 13:26:02 +0300

    superb-kitchen

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,97 +1,76 @@
-import React, {useState, useEffect} from 'react'
-import './reg-log.css'
+import React, { useState, useEffect } from "react";
+import "./reg-log.css";
 import pic5 from "../../images/kitchen-5.jpg";
 import PhoneInput from "react-phone-number-input";
 import "react-phone-number-input/style.css";
 import { AiOutlineEye } from "react-icons/ai";
-import {Link} from 'react-router-dom'
-function Register(){
+import { Link } from "react-router-dom";
+function Register() {
+  const [contact, setContact] = useState("+254");
+  const handleContact = (phoneNumber) => {
+    setContact(phoneNumber);
+  };
 
-    const [contact, setContact] = useState("+254");
-    const handleContact = (phoneNumber) => {
-        setContact(phoneNumber);
-      };
+  return (
+    <>
+      <section className="register">
+        <div className="regsiter-container">
+          <p className="home-link">Superb Kitchen</p>
+          <p>Register</p>
 
-    return(
-        <>
+          <form>
+            <div className="form-container">
+              <div className="name-label">
+                <label>Name</label>
+              </div>
+              <div className="input-container">
+                <input type="text" placeholder="enter your name" />
+              </div>
+            </div>
 
-            <section className='register'>
+            <div className="form-container">
+              <div className="name-label">
+                <label>Email</label>
+              </div>
+              <div className="input-container">
+                <input type="email" placeholder="enter your Email" />
+              </div>
+            </div>
 
-                <div className='regsiter-container'>
-
-                    <p className="home-link">Superb Kitchen</p>
-                    <p>Register</p>
-
-                    <form>
-
-                        <div className='form-container'>
-                            <div className='name-label'>
-                                <label>Name</label>
-
-                            </div>
-                            <div className='input-container'>
-                                <input type='text' placeholder='enter your name'/>
-                            </div>
-                        </div>
-
-                        <div className='form-container'>
-                            <div className='name-label'>
-                                <label>Email</label>
-
-                            </div>
-                            <div className='input-container'>
-                                <input type='email' placeholder='enter your Email'/>
-                            </div>
-                        </div>
-                        
-                        <div className='form-container'>
-                            {/* <div className='name-label'>
+            <div className="form-container">
+              {/* <div className='name-label'>
                                 <label>Contact</label>
 
                             </div> */}
-                            <div className='input-container'>
-                            <PhoneInput
-                placeholder="Your Phone Number"
-                className="pwd-input"
-                onChange={handleContact}
-                value={contact}
-              />
-                            </div>
-                        </div>
+              <div className="input-container">
+                <PhoneInput
+                  placeholder="Your Phone Number"
+                  className="pwd-input"
+                  onChange={handleContact}
+                  value={contact}
+                />
+              </div>
+            </div>
 
-                        <div className='form-container'>
-                            <div className='name-label'>
-                                <label>Password</label>
+            <div className="form-container">
+              <div className="name-label">
+                <label>Password</label>
+              </div>
+              <div className="input-container">
+                <input type="password" placeholder="enter your Password" />
+                <AiOutlineEye className="toggle-password" />
+              </div>
+            </div>
 
-                            </div>
-                            <div className='input-container'>
-                                <input type='password' placeholder='enter your Password'/>
-                                <AiOutlineEye className="toggle-password"  />
-                            </div>
-                        </div>
-
-                        <button type='submit'>Let's Go</button>
-                        <Link to="/login" className="login-p">
+            <button type="submit">Let's Go</button>
+            <Link to="/login" className="login-p">
               <p>Already Registered? Sign-In</p>
             </Link>
-
-
-                    </form>
-
-
-
-
-
-                </div>
-                
-
-
-
-            </section>
-        
-        
-        </>
-    )
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
