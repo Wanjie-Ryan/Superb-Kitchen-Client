1a14b249ff0263bc36a40b40cc9d063f481ec06a
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-16 13:38:20 +0300

    cart

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './cart.css'
+import pic1 from "../../images/kitchen-1.jpg";
 
 
 
@@ -8,6 +9,46 @@ function Cart(){
     return(
 
         <>
+
+            <section className='cart'>
+
+                <div className='cart-container'>
+
+
+
+                <div className='cart-inner'>
+
+                    <div className='cart-image-container'>
+                    <img src={pic1} className='cart-image' alt='cart-image'/>
+                    </div>
+
+
+
+                </div>
+
+                <div className='cart-details-right'>
+
+                    <div className='cart-container-details'>
+                    <p className="item-name">Gold Spoons</p>
+                <p className="item-price">Ksh.1200</p>
+                <p className="item-count">20 remaining</p>
+                <button className="item-btn">
+                  {/* <Link to="/cart" className="cart-link"> */}
+                    {/* <BsFillCartPlusFill className="cart-icon" /> */}
+                    Proceed to Checkout
+                  {/* </Link> */}
+                </button>
+
+                    </div>
+
+
+                </div>
+
+
+
+
+                </div>
+            </section>
 
         </>
     )
