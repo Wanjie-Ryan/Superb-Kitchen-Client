c9c1a2064cc55fd500640e336d02a0a2e2f1d2d8
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-18 14:49:25 +0300

    cart

--- src/components/context/cart.js
+++ src/components/context/cart.js
@@ -1,31 +1,31 @@
-import React, {useContext, createContext, useState} from 'react'
+import React, { useContext, createContext, useState } from "react";
 
-const cartContext = createContext()
+const cartContext = createContext();
 
-export function useCart(){
-    return useContext(cartContext)
+export function useCart() {
+  return useContext(cartContext);
 }
 
-export function CartProvider({children}) {
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
 
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
 
-    const [cart, setCart] = useState([])
+  const removeFromCart = (productId) => {
+    setCart(cart.filter((item) => item.id !== productId));
+  };
 
-    const addToCart = (product) => {
-        setCart([...cart, product])
-    }
+  const clearCart = () => {
+    setCart([]);
+  };
 
-    const removeFromCart = (productId) => {
-        setCart(cart.filter(item => item.id !== productId))
-    }
-
-    const clearCart = () => {
-        setCart([])
-    }
-
-    return (
-        <cartContext.Provider value={{cart, addToCart, removeFromCart, clearCart}}>
-            {children}
-        </cartContext.Provider>
-    )
+  return (
+    <cartContext.Provider
+      value={{ cart, addToCart, removeFromCart, clearCart }}
+    >
+      {children}
+    </cartContext.Provider>
+  );
 }
