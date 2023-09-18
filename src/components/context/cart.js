1609b6c4f20fa16945bbb4061e3e7d8585585dd3
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-18 15:01:54 +0300

    cart

--- src/components/context/cart.js
+++ src/components/context/cart.js
@@ -1,13 +1,17 @@
 import React, { useContext, createContext, useState } from "react";
 
 const cartContext = createContext();
+// cartContext holds the state and functions related to shopping cart, making them accessible to components within its provider
 
 export function useCart() {
   return useContext(cartContext);
 }
+// useCart function allows components to easily access the cart state and functions, it uses the useContext hook to retrieve values from the cartContext
 
 export function CartProvider({ children }) {
+  //cartProvider acts as a provider for the cart context, it wraps the entire app to make the  cart state and functions available to its child components.
   const [cart, setCart] = useState([]);
+  //initializes the cart state to an empty array, it will store an array of products in the shopping cart
 
   const addToCart = (product) => {
     setCart([...cart, product]);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { CartProvider } from "../src/components/context/cart";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>
 );
-
-
