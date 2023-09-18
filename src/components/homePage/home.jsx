83272fc4e8b2065f681b2be23d3200fbccbaf077
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-18 14:33:50 +0300

    cart

--- src/components/homePage/home.jsx
+++ src/components/homePage/home.jsx
@@ -8,178 +8,81 @@ import pic5 from "../../images/kitchen-5.jpg";
 import { BsFillCartPlusFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 function Home() {
-
   const imageData = [
     {
-      id:1,
-      pic:require('../../images/kitchen-1.jpg'),
-      name:'Gold Spoons',
-      price:'ksh.1200',
-      quantity:'20 remaining',
-      button:'Add To cart'
+      id: 1,
+      pic: require("../../images/kitchen-1.jpg"),
+      name: "Gold Spoons",
+      price: "ksh.1200",
+      quantity: "20 remaining",
+      button: "Add To cart",
     },
     {
-      id:2,
-      pic:require('../../images/kitchen-2.jpg'),
-      name:'Gold Spoons',
-      price:'ksh.1200',
-      quantity:'20 remaining',
-      button:'Add To cart'
+      id: 2,
+      pic: require("../../images/kitchen-2.jpg"),
+      name: "Gold Spoons",
+      price: "ksh.1200",
+      quantity: "20 remaining",
+      button: "Add To cart",
     },
     {
-      id:3,
-      pic:require('../../images/kitchen-3.jpg'),
-      name:'Gold Spoons',
-      price:'ksh.1200',
-      quantity:'20 remaining',
-      button:'Add To cart'
+      id: 3,
+      pic: require("../../images/kitchen-3.jpg"),
+      name: "Gold Spoons",
+      price: "ksh.1200",
+      quantity: "20 remaining",
+      button: "Add To cart",
     },
     {
-      id:4,
-      pic:require('../../images/kitchen-4.jpg'),
-      name:'Gold Spoons',
-      price:'ksh.1200',
-      quantity:'20 remaining',
-      button:'Add To cart'
+      id: 4,
+      pic: require("../../images/kitchen-4.jpg"),
+      name: "Gold Spoons",
+      price: "ksh.1200",
+      quantity: "20 remaining",
+      button: "Add To cart",
     },
     {
-      id:5,
-      pic:require('../../images/kitchen-5.jpg'),
-      name:'Gold Spoons',
-      price:'ksh.1200',
-      quantity:'20 remaining',
-      button:'Add To cart'
+      id: 5,
+      pic: require("../../images/kitchen-5.jpg"),
+      name: "Gold Spoons",
+      price: "ksh.1200",
+      quantity: "20 remaining",
+      button: "Add To cart",
     },
     {
-      id:6,
-      pic:require('../../images/kitchen-5.jpg'),
-      name:'Gold Spoons',
-      price:'ksh.1200',
-      quantity:'20 remaining',
-      button:'Add To cart'
-    }
-  ]
+      id: 6,
+      pic: require("../../images/kitchen-5.jpg"),
+      name: "Gold Spoons",
+      price: "ksh.1200",
+      quantity: "20 remaining",
+      button: "Add To cart",
+    },
+  ];
   return (
     <>
       <section className="home">
         <div className="home-inner">
+          {imageData.map((items) => (
+            <div className="kitchen-items">
+              <div className="kitchen-details" key={items.id}>
+                <div className="img-container">
+                  <img src={items.pic} alt="image" className="product-image" />
+                </div>
 
-          {imageData.map((items)=>(
-            <div className="kitchen-items" >
-            <div className="kitchen-details" key={items.id}>
-              <div className="img-container">
-                <img src={items.pic} alt="image" className="product-image" />
-              </div>
-
-              <div className="img-desc">
-                <p className="item-name">{items.name}</p>
-                <p className="item-price">{items.price}</p>
-                <p className="item-count">{items.quantity}</p>
-                <button className="item-btn">
-                  <Link to="/cart" className="cart-link">
-                    <BsFillCartPlusFill className="cart-icon" />
-                    {items.button}
-                  </Link>
-                </button>
-              </div>
-          </div>
-            </div>
-          ))
-}
-
-          {/* <div className="kitchen-items">
-            <div className="kitchen-details">
-              <div className="img-container">
-                <img src={pic1} alt="image" className="product-image" />
-              </div>
-
-              <div className="img-desc">
-                <p className="item-name">Gold Spoons</p>
-                <p className="item-price">Ksh.1200</p>
-                <p className="item-count">20 remaining</p>
-                <button className="item-btn">
-                  <BsFillCartPlusFill className="cart-icon" />
-                  Add To cart
-                </button>
+                <div className="img-desc">
+                  <p className="item-name">{items.name}</p>
+                  <p className="item-price">{items.price}</p>
+                  <p className="item-count">{items.quantity}</p>
+                  <button className="item-btn">
+                    <Link to="/cart" className="cart-link">
+                      <BsFillCartPlusFill className="cart-icon" />
+                      {items.button}
+                    </Link>
+                  </button>
+                </div>
               </div>
             </div>
-          </div> */}
-
-          {/* <div className="kitchen-items">
-            <div className="kitchen-details">
-              <div className="img-container">
-                <img src={pic2} alt="image" className="product-image" />
-              </div>
-
-              <div className="img-desc">
-                <p className="item-name">Gold Spoons</p>
-                <p className="item-price">Ksh.1200</p>
-                <p className="item-count">20 remaining</p>
-                <button className="item-btn">
-                  <BsFillCartPlusFill className="cart-icon" />
-                  Add To cart
-                </button>
-              </div>
-            </div>
-          </div> */}
-
-          {/* <div className="kitchen-items">
-            <div className="kitchen-details">
-              <div className="img-container">
-                <img src={pic3} alt="image" className="product-image" />
-              </div>
-
-              <div className="img-desc">
-                <p className="item-name">Gold Spoons</p>
-                <p className="item-price">Ksh.1200</p>
-                <p className="item-count">20 remaining</p>
-                <button className="item-btn">
-                  <BsFillCartPlusFill className="cart-icon" />
-                  Add To cart
-                </button>
-              </div>
-            </div>
-          </div> */}
-
-          {/* <div className="kitchen-items">
-            <div className="kitchen-details">
-              <div className="img-container">
-                <img src={pic4} alt="image" className="product-image" />
-              </div>
-
-              <div className="img-desc">
-                <p className="item-name">Gold Spoons</p>
-                <p className="item-price">Ksh.1200</p>
-                <p className="item-count">20 remaining</p>
-                <button className="item-btn">
-                  <Link to="/cart" className="cart-link">
-                    <BsFillCartPlusFill className="cart-icon" />
-                    Add To cart
-                  </Link>
-                </button>
-              </div>
-            </div>
-          </div> */}
-
-          {/* <div className="kitchen-items">
-            <div className="kitchen-details">
-              <div className="img-container">
-                <img src={pic5} alt="image" className="product-image" />
-              </div>
-
-              <div className="img-desc">
-                <p className="item-name">Gold Spoons</p>
-                <p className="item-price">Ksh.1200</p>
-                <p className="item-count">20 remaining</p>
-                <button className="item-btn">
-                  <Link to="/cart" className="cart-link">
-                    <BsFillCartPlusFill className="cart-icon" />
-                    Add To cart
-                  </Link>
-                </button>
-              </div>
-            </div>
-          </div> */}
+          ))}
         </div>
       </section>
     </>
