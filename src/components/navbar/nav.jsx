df5569def26882eec9c65f023a0b9107a16e38d6
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-24 13:38:33 +0300

    user logics

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,17 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./nav.css";
-import { Link } from "react-router-dom";
-import { BsChatDots, BsPersonCircle, BsFillCartFill } from "react-icons/bs";
+import { Link, useNavigate } from "react-router-dom";
+import { BsPersonCircle, BsFillCartFill } from "react-icons/bs";
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import Cookies from "js-cookie";
 import { useCart } from "../context/cart";
+import { ToastContainer, toast } from "react-toastify";
 
 function Navbar() {
   const [asideOpen, setAsideOpen] = useState(false);
   const toggleAside = () => {
     setAsideOpen(!asideOpen);
   };
+
+  const LogDetails = JSON.parse(sessionStorage.getItem("UserLoginDetails"));
+
+  let username;
+
+  if (LogDetails) {
+    username = LogDetails.name;
+  }
+
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour >= 5 && currentHour < 12) {
+      setGreeting("Good Morning");
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting("Good Afternoon");
+    } else {
+      setGreeting("Good Evening");
+    }
+  }, []);
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    Cookies.remove("userToken");
+    toast.success('Till next time')
+    
+
+    navigate("/");
+  };
+
+
 
   const { cart } = useCart();
 
@@ -47,7 +81,7 @@ function Navbar() {
             </div>
 
             <div className="nav-main__right__logout">
-              <p>Logout</p>
+              <p onClick={handleLogout}>Logout</p>
             </div>
 
             <div className="nav-main__right__profile">
@@ -58,7 +92,7 @@ function Navbar() {
                   <BsPersonCircle />
                 )}
               </Link>
-              <p>Good Afternoon Kibet</p>
+              <p>{greeting} {username}</p>
             </div>
 
             <div className="nav-main__right__dashboard-menu">
@@ -106,11 +140,11 @@ function Navbar() {
                   <BsPersonCircle />
                 )}
               </Link>
-              <p>Good Afternoon Kibet</p>
+              <p>{greeting} {username}</p>
             </div>
 
             <div className="main-aside__inner__top__logout">
-              <p>Logout</p>
+              <p onClick={handleLogout}>Logout</p>
             </div>
           </div>
         </div>
