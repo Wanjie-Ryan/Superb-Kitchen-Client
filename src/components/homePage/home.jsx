c8c4ee335f3cd88359144ad644e552ef489c1c3e
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-24 07:58:12 +0300

    vendor logics

--- src/components/homePage/home.jsx
+++ src/components/homePage/home.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./home.css";
 import { BsFillCartPlusFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { useCart } from "../context/cart";
+import axios from 'axios'
+import {  AiOutlineLoading3Quarters } from "react-icons/ai";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function Home() {
   const { addToCart, cartClicked } = useCart();
 
@@ -57,6 +62,45 @@ function Home() {
     },
   ];
 
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(()=>{
+
+    const getUserProducts  = async()=>{
+
+      try{
+
+        setLoading(true)
+        const response = await axios.get('http://localhost:3005/api/user/products/userproducts')
+        // console.log(response.data.userProducts)
+        setProducts(response.data.userProducts)
+        setLoading(false)
+
+
+      }
+
+      catch(err){
+
+        console.log(err)
+        if(err.response.status === 500){
+          toast.error('A problem with our servers, hang on')
+        }
+      }
+      finally{
+        setLoading(false)
+      }
+
+
+
+    }
+
+    getUserProducts()
+
+
+
+  },[])
+
   return (
     <>
       <section className="home">
@@ -69,6 +113,7 @@ function Home() {
                 </div>
 
                 <div className="img-desc">
+                  
                   <p className="item-name">{items.name}</p>
                   <p className="item-price">{items.price}</p>
                   <p className="item-count">{items.quantity}</p>
