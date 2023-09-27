05f0dbb4825205331ad0f83ed4c0f122948fc04b
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-27 07:01:07 +0300

    pays

--- src/components/cart/checkoutModal.jsx
+++ src/components/cart/checkoutModal.jsx
@@ -2,25 +2,26 @@ import React, { useState, useEffect } from "react";
 import "./checkoutModal.scss";
 import Modal from "react-modal";
 import { AiOutlineClose } from "react-icons/ai";
-import axios from 'axios'
+import axios from "axios";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 
-function Checkout({ isOpen, onClose, selectedItem,token }) {
+function Checkout({ isOpen, onClose, selectedItem, token }) {
   // console.log(selectedItem.createdBy.Vendor)
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [name, setName] = useState();
   const [contact, setContact] = useState();
   const [email, setEmail] = useState();
-  const [location, setLocation] = useState()
+  const [location, setLocation] = useState();
   const [quantity, setQuantity] = useState();
   const [amount, setAmount] = useState();
-  const [vendor, setVendor] = useState([])
-  // console.log(vendor.contact)
-  const phone = vendor.contact
+  const [vendor, setVendor] = useState([]);
+  // console.log(vendor._id)
+  const phone = vendor.contact;
+  // console.log(phone)
 
   const handleName = (e) => {
     setName(e.target.value);
@@ -32,13 +33,12 @@ function Checkout({ isOpen, onClose, selectedItem,token }) {
     setEmail(e.target.value);
   };
 
-  const handleLocation = (e)=>{
-    setLocation(e.target.value)
-  }
+  const handleLocation = (e) => {
+    setLocation(e.target.value);
+  };
   const handleQuantity = (e) => {
     setQuantity(e.target.value);
   };
-  
 
   useEffect(() => {
     if (quantity && selectedItem) {
@@ -49,39 +49,40 @@ function Checkout({ isOpen, onClose, selectedItem,token }) {
       setAmount(0);
     }
 
-    if(selectedItem){
-
-      axios.get(`http://localhost:3005/api/vendor/auth/singleVendor/${selectedItem.createdBy}`, {headers:{Authorization :"Bearer " + token}}).then((response)=>{
-        setVendor(response.data.VendorObj)
-      }).catch((err)=>{
-        console.log(err)
-        if (err.response.status === 401) {
-          toast.error("Not Authorized");
-          navigate("/");
-        } else if (err.response.status === 404) {
-          toast.error("Vendor not found");
-        } else if (err.response.status === 500) {
-          toast.error("A problem with our servers, hang on");
-        }
-      })
-
-
+    if (selectedItem) {
+      axios
+        .get(
+          `http://localhost:3005/api/vendor/auth/singleVendor/${selectedItem.createdBy}`,
+          { headers: { Authorization: "Bearer " + token } }
+        )
+        .then((response) => {
+          setVendor(response.data.VendorObj);
+        })
+        .catch((err) => {
+          console.log(err);
+          if (err.response.status === 401) {
+            toast.error("Not Authorized");
+            navigate("/");
+          } else if (err.response.status === 404) {
+            toast.error("Vendor not found");
+          } else if (err.response.status === 500) {
+            toast.error("A problem with our servers, hang on");
+          }
+        });
     }
   }, [quantity, selectedItem]);
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-  const handlePayment = async(e)=>{
-    e.preventDefault()
+  const handlePayment = async (e) => {
+    e.preventDefault();
 
-    try{
+    try {
+      setLoading(true);
 
-      setLoading(true)
-      
-      if(!name || !contact || !email ||!location || !quantity){
-        
-        toast.error('Please fill the required fields')
-        return
+      if (!name || !contact || !email || !location || !quantity) {
+        toast.error("Please fill the required fields");
+        return;
       }
 
       const customer_details = {
@@ -94,8 +95,8 @@ function Checkout({ isOpen, onClose, selectedItem,token }) {
       const products = [
         {
           product_name: selectedItem.name,
-          quantity: quantity,
-          unit_price: selectedItem.price,
+          quantity: parseInt(quantity),
+          unit_price: parseFloat(selectedItem.price),
         },
       ];
 
@@ -110,28 +111,30 @@ function Checkout({ isOpen, onClose, selectedItem,token }) {
         },
         callback_details: {
           notify_customer: true,
-          transaction_reference: vendor._id,
+          transaction_reference: selectedItem._id,
           callback_url:
             "https://8903-154-79-248-134.ngrok-free.app/api/chpter/payment",
         },
       };
-      
 
-      const response = await axios.post('https://api.chpter.co/v1/initiate/mpesa-payment', Payment_Data, {headers:{ "Content-Type": "application/json",
-      "Api-key":
-        "pk_test_d39abf62f0e7f5b39fd55bcd19579aaa7caeab3ba6dd59c80d2c4ba5df38a106",}})
+      const response = await axios.post(
+        "https://api.chpter.co/v1/initiate/mpesa-payment",
+        Payment_Data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Api-key":
+              "pk_test_d39abf62f0e7f5b39fd55bcd19579aaa7caeab3ba6dd59c80d2c4ba5df38a106",
+          },
+        }
+      );
 
-        console.log(response)
+      console.log(response);
 
-        setLoading(false)
-        toast.success("An STK-Push has been sent to your phone, confirm");
-
-
-      
-    }
-
-    catch(err){
-      console.log(err)
+      setLoading(false);
+      toast.success("An STK-Push has been sent to your phone, confirm");
+    } catch (err) {
+      console.log(err);
       if (err.response.status === 401) {
         toast.error("Payment not authorized");
       } else if (err.response.status === 400) {
@@ -139,15 +142,10 @@ function Checkout({ isOpen, onClose, selectedItem,token }) {
       } else {
         toast.error("A problem with the servers, hang on");
       }
+    } finally {
+      setLoading(false);
     }
-
-    finally{
-
-      setLoading(false)
-    }
-
-
-  }
+  };
 
   return (
     <>
@@ -201,7 +199,12 @@ function Checkout({ isOpen, onClose, selectedItem,token }) {
 
             <div className="contact__contact-container__name">
               <label>Location</label>
-              <input type="text" placeholder="eg.Nairobi" value={location} onChange={handleLocation}/>
+              <input
+                type="text"
+                placeholder="eg.Nairobi"
+                value={location}
+                onChange={handleLocation}
+              />
             </div>
 
             <div className="contact__contact-container__name">
@@ -244,9 +247,8 @@ function Checkout({ isOpen, onClose, selectedItem,token }) {
                 className="price"
               />
             </div>
-          <button className="contact__okay-btn">Pay</button>
+            <button className="contact__okay-btn">Pay</button>
           </form>
-
         </main>
       </Modal>
     </>
