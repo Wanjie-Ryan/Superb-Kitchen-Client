2b28d3c7f5625001fefbd88e03207fa64cee129b
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-28 10:13:28 +0300

    pays

--- src/components/records/records.jsx
+++ src/components/records/records.jsx
@@ -1,13 +1,70 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./records.css";
 import { FaFileCsv } from "react-icons/fa";
 import { BsFillFileEarmarkPdfFill } from "react-icons/bs";
 import { payDataContext } from "../context/pay";
+import axios from 'axios'
+import Cookies from "js-cookie";
+import {useNavigate} from 'react-router-dom'
+import { ToastContainer, toast } from "react-toastify";
 
 function Records() {
   const { payd } = useContext(payDataContext);
+  // console.log(payd);
+  const productId = payd? payd.latestPayment.transaction_reference:''
+  console.log(productId)
 
-  console.log(payd);
+
+  const [product, setProduct] = useState([])
+  const [recordIndex, setRecordIndex] = useState(0)
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+
+  const token  = Cookies.get('userToken')
+
+
+    const getSingleProduct = async()=>{
+
+      try{
+
+
+        
+        const response = await axios.get(`http://localhost:3005/api/user/products/usersingleproduct${productId}`, {headers:{Authorization: "Bearer" + token}})
+        
+      console.log(response)
+
+      setProduct(response.singleProduct)
+    }
+    catch(err){
+
+      console.log(err)
+
+      if(err.response.status === 401){
+        toast.error('Not Authorized')
+        navigate('/')
+      }
+      else if(err.response.status === 404 ){
+        toast.error('Product Not found')
+      }
+      else if(err.response.status === 500){
+        toast.error("A problem with our servers, hang on")
+      }
+    }
+
+
+
+
+
+    }
+
+    getSingleProduct()
+
+
+
+  },[productId])
+
+
 
   return (
     <>
@@ -27,7 +84,7 @@ function Records() {
               <tr>
                 <th>No</th>
                 <th>Product</th>
-                <th>Quantity</th>
+                {/* <th>Quantity</th> */}
                 <th>Amount</th>
                 <th>Payment Status</th>
                 <th>Delivery Status</th>
@@ -35,10 +92,10 @@ function Records() {
             </thead>
             <tbody>
               <tr>
-                <td>1</td>
-                <td>Spoons</td>
-                <td>3</td>
-                <td>Ksh. {payd.latestPayment.Amount[0]}</td>
+                <td>{recordIndex +1}</td>
+                <td>{product? product.name:'product'}</td>
+                {/* <td>3</td> */}
+                <td>Ksh. {payd?payd.latestPayment.Amount[0]:'0'}</td>
                 <td className="paid">Paid</td>
                 <td className="pending">Pending</td>
               </tr>
