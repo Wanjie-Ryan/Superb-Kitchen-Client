b075152b8c51a8a77132c2d9523d0bfeff3c3979
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-28 10:16:08 +0300

    pays

--- src/components/records/records.jsx
+++ src/components/records/records.jsx
@@ -3,68 +3,50 @@ import "./records.css";
 import { FaFileCsv } from "react-icons/fa";
 import { BsFillFileEarmarkPdfFill } from "react-icons/bs";
 import { payDataContext } from "../context/pay";
-import axios from 'axios'
+import axios from "axios";
 import Cookies from "js-cookie";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 
 function Records() {
   const { payd } = useContext(payDataContext);
   // console.log(payd);
-  const productId = payd? payd.latestPayment.transaction_reference:''
-  console.log(productId)
+  const productId = payd ? payd.latestPayment.transaction_reference : "";
+  console.log(productId);
 
+  const [product, setProduct] = useState([]);
+  const [recordIndex, setRecordIndex] = useState(0);
+  const navigate = useNavigate();
 
-  const [product, setProduct] = useState([])
-  const [recordIndex, setRecordIndex] = useState(0)
-  const navigate = useNavigate()
+  useEffect(() => {
+    const token = Cookies.get("userToken");
 
-  useEffect(()=>{
+    const getSingleProduct = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3005/api/user/products/usersingleproduct${productId}`,
+          { headers: { Authorization: "Bearer" + token } }
+        );
 
-  const token  = Cookies.get('userToken')
+        console.log(response);
 
+        setProduct(response.singleProduct);
+      } catch (err) {
+        console.log(err);
 
-    const getSingleProduct = async()=>{
-
-      try{
-
-
-        
-        const response = await axios.get(`http://localhost:3005/api/user/products/usersingleproduct${productId}`, {headers:{Authorization: "Bearer" + token}})
-        
-      console.log(response)
-
-      setProduct(response.singleProduct)
-    }
-    catch(err){
-
-      console.log(err)
-
-      if(err.response.status === 401){
-        toast.error('Not Authorized')
-        navigate('/')
+        if (err.response.status === 401) {
+          toast.error("Not Authorized");
+          navigate("/");
+        } else if (err.response.status === 404) {
+          toast.error("Product Not found");
+        } else if (err.response.status === 500) {
+          toast.error("A problem with our servers, hang on");
+        }
       }
-      else if(err.response.status === 404 ){
-        toast.error('Product Not found')
-      }
-      else if(err.response.status === 500){
-        toast.error("A problem with our servers, hang on")
-      }
-    }
+    };
 
-
-
-
-
-    }
-
-    getSingleProduct()
-
-
-
-  },[productId])
-
-
+    getSingleProduct();
+  }, [productId]);
 
   return (
     <>
@@ -92,10 +74,10 @@ function Records() {
             </thead>
             <tbody>
               <tr>
-                <td>{recordIndex +1}</td>
-                <td>{product? product.name:'product'}</td>
+                <td>{recordIndex + 1}</td>
+                <td>{product ? product.name : "product"}</td>
                 {/* <td>3</td> */}
-                <td>Ksh. {payd?payd.latestPayment.Amount[0]:'0'}</td>
+                <td>Ksh. {payd ? payd.latestPayment.Amount[0] : "0"}</td>
                 <td className="paid">Paid</td>
                 <td className="pending">Pending</td>
               </tr>
